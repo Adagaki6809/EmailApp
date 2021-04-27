5edb47dad66ee39d7f8c1a88a4252d323393b6f8
Hitagi <include.ofstream.h@gmail.com>
2021-04-27 17:39:30 +0300

    first commit

---
+++ Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmailApp.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
